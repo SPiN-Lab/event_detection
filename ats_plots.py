4194bb3ecb2d5638c055dfd4cc4fb89824915c86
Vicente Ferrer <v.ferrer@bcbl.eu>
2021-06-24 11:46:19 +0200

    Update ats_plots.py

--- ats_plots.py
+++ ats_plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 prj_dir = "/bcbl/home/public/PARK_VFERRER/PFM_data/sub-002ParkMabCm_200"
-temp_dir = "/export/home/eurunuela/public/PARK_VFERRER/PFM_data/temp_sub-002ParkMabCm_200"
+temp_dir = "/bcbl/home/public/PARK_VFERRER/PFM_data/temp_sub-002ParkMabCm_200"
 n_rand = 100
 fig_size = (16, 9)
 ats_mode = "power"
